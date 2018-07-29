ed0ca88749ed9e66c6619963538ab271149015d7
Dan Hopper <dhopper2@sbcglobal.net>
2018-07-28 20:01:33 -0500

    Moved css folder to public to solve MIME error

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ const apiRoutes = require('./app/routing/apiRoutes')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(path.join(__dirname +'/app/'))
+app.use(express.static(path.join(__dirname, '/app/public')))
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/', htmlRoutes);
 app.use('/', apiRoutes);
+console.log(__dirname)
+
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
